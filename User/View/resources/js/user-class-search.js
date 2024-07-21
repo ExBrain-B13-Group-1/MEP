e35f4a4984f2025fd4308e766fb6522a2fb3008d
ThihaThwin-Dev <Thihathwindeveloper@gmail.com>
2024-07-22 00:01:36 +0630

    latest update

--- User/View/resources/js/user-class-search.js
+++ User/View/resources/js/user-class-search.js
@@ -12,3 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     new Splide("#instructorSplide").mount();
 });
+
+$(document).ready(function() {
+    $("#seeMoreInstituteButton").click(function() {
+        if ($("#innerInstituteWrapper").hasClass("active-wrapper")) {
+            $("#innerInstituteWrapper").removeClass("active-wrapper");
+            setTimeout(function() {
+                $("#seeMoreButton").text("See more");
+            }, 800);
+        } else {
+            $("#innerInstituteWrapper").addClass("active-wrapper");
+            $("#seeMoreButton").text("See less");
+        }
+    });
+    $("#seeMoreClassButton").click(function() {
+        if ($("#innerClassWrapper").hasClass("active-wrapper")) {
+            $("#innerClassWrapper").removeClass("active-wrapper");
+            setTimeout(function() {
+                $("#seeMoreButton").text("See more");
+            }, 800);
+        } else {
+            $("#innerClassWrapper").addClass("active-wrapper");
+            $("#seeMoreButton").text("See less");
+        }
+    });
+});
+
