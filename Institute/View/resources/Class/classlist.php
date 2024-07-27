f1a9b38b5a78ebe244913e6f062205c7a5f9e205
Saw Phyo Naing <168881414+sawphyonaingdev@users.noreply.github.com>
2024-07-27 13:46:27 +0630

    git test

--- Institute/View/resources/Class/classlist.php
+++ Institute/View/resources/Class/classlist.php
@@ -370,7 +370,7 @@
                     <div class="relative overflow-x-auto shadow-md dark:shadow-none sm:rounded-lg">
                         <!-- ClassList Table -->
                         <div id="data-container">
-                            <table class=" w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-400 dark:bg-gray-700 duration-500 dark:text-gray-400">
                                     <th scope="col" class="p-4">
                                         ID
